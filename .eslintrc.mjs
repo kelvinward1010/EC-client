6175dba0311cadc5727a3afc28eed2ff370507f7
Kelvin Ward <kelvinward1010@gmail.com>
2024-07-27 16:18:17 +0700

    first commit

--- .eslintrc.mjs
+++ .eslintrc.mjs
@@ -5,11 +5,14 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "plugin:prettier/recommended",
+    "prettier"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
+    "prettier/prettier": ["warn"],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
