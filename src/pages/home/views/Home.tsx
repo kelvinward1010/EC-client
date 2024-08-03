e0a9223df6e8b5649866061a1dbfffd02b22c711
Kelvin Ward <kelvinward1010@gmail.com>
2024-08-03 17:14:39 +0700

    updated home

--- src/pages/home/views/Home.tsx
+++ src/pages/home/views/Home.tsx
@@ -1,3 +1,4 @@
+import Fashion from "../components/Fashion";
 import HotProduct from "../components/HotProduct";
 import TopHeadHome from "../components/TopHeadHome";
 import styles from "./Home.module.scss";
@@ -7,6 +8,7 @@ export function Home() {
         <div className={styles.container}>
             <TopHeadHome />
             <HotProduct />
+            <Fashion />
         </div>
     );
 }
